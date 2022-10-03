10301e629b9cc53ccc03f7c9c6f180dbbdca9be2
AdryDevs <alonsoadrian86@gmail.com>
2022-10-03 19:49:14 +0200

    retos del 1 al 5 realizados

---
+++ Retos.js
@@ -0,0 +1,43 @@
+/* 1.- Declara 2 variables numéricas (con el valor que desees), indica cual es mayor de
+ los dos. Si son iguales indicalo también. Puedes ir cambiando los valores para comprobar que funciona. */
+
+   //  var num1 = 12;
+   //  var num2 = 9;
+
+   //  if (num1 < num2) {
+   //     console.log("El primero número es menor que el segundo")
+   //  } else if (num1 > num2) {
+   //     console.log("El segundo número es menor que el primero")
+   //  } else {
+   //     console.log("Los números son iguales")
+   //  };
+
+//  2.- Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida por consola. Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
+
+   // var name = "Adrián";
+
+   // console.log("Bienvenido " + name)
+
+// 3.- Modifica la aplicación anterior, para que nos pida el nombre que queremos introducir (recuerda usar prompt).
+
+   // var name = prompt("Introduce tu nombre");
+
+   // console.log("Bienvenido " + name);
+
+// 4.- Haz una aplicación que calcule el área de un círculo (pi*R2). El radio se pedirá por teclado (recuerda pasar de String a double con parseDouble). Usa la constante PI.
+
+   // var radio = prompt("Introduce el radio del círculo");
+
+   // var area = Math.PI * Math.pow(radio, 2);
+
+   // console.log ("El area es " + area);
+
+// 5.-Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es, también debemos indicarlo.
+
+   // var num = prompt("Introduce un número");
+
+   // if (num%2 == 0) {
+   //    console.log ("El resto da 0, el número es divisible entre 2")
+   // } else {
+   //    console.log("El resto no da 0, el número no es divisible entre 2")
+   // };
