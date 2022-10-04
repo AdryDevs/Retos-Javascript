43834e1fb4c705ca802d26b2cffc25151c439dec
AdryDevs <alonsoadrian86@gmail.com>
2022-10-04 19:48:17 +0200

    todos los retos terminados.

--- Retos.js
+++ Retos.js
@@ -16,8 +16,8 @@
 
    // var name = "Adrián";
 
-   // console.log("Bienvenido " + name)
-
+   // console.log("Bienvenido ", name)  la coma ya da un espacio. Es mejor utilizar coma porque respeta si es string o number el de después.
+   
 // 3.- Modifica la aplicación anterior, para que nos pida el nombre que queremos introducir (recuerda usar prompt).
 
    // var name = prompt("Introduce tu nombre");
@@ -41,3 +41,149 @@
    // } else {
    //    console.log("El resto no da 0, el número no es divisible entre 2")
    // };
+
+// 6.- Lee un número por teclado que pida el precio de un producto (puede tener decimales) y calcule el precio final con IVA. El IVA sera una constante que sera del 21%
+
+   // let num1 = parseFloat(prompt("Introduce un precio"));
+   // const IVA = 0.21;
+
+   // function calcularIVA() {
+
+   //    let precioConIVA = IVA * num1 + num1;
+
+
+   
+   //    return console.log("El precio con IVA es de ",precioConIVA, "€")
+   // }
+
+   // calcularIVA();
+
+
+   // 7.- Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
+
+   // let i = 0;
+
+   // while (i < 100) {
+   //    i++;
+   //    console.log(i);
+   // }
+
+
+   // 8.- Haz el mismo ejercicio anterior con un bucle for.
+
+   // for (let i=1; i <= 100; i++) {
+   //    console.log(i);
+   // }
+
+   // 9.- Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3. Utiliza el bucle que desees.
+
+   // for (let i = 1; i <= 100; i++){
+   //    if ((i % 2 == 0) && (i % 3 ==0)){
+   //       console.log (i)
+   //    }
+   // }
+
+   // 10.- Realiza una aplicación que nos pida un número de ventas a introducir, después nos pedirá tantas ventas por teclado como número de ventas se hayan indicado. Al final mostrara la suma de todas las ventas. Piensa que es lo que se repite y lo que no.
+
+   // let i = parseInt(prompt("Introduce número de ventas"));
+   
+   // let total = "";
+
+   // while (i > 0) {
+   //    i--;
+   //    let venta = parseInt(prompt("Introduce un precio de producto"));
+   //    total = parseInt(total + venta);
+      
+   // }
+
+   // console.log(total);
+
+   // 11.- Crea una aplicación que nos pida un día de la semana y que nos diga si es un día laboral o no. Usa un switch para ello.
+
+   // let diaSemana = prompt("Introduce un día de la semana para saber si es laboral o no");
+
+   // switch (diaSemana) {
+   //    case "Lunes":
+   //    case "Martes":
+   //    case "Miércoles":
+   //    case "Jueves":
+   //    case "Viernes":  
+   //       console.log("Es un día laboral")       
+   //       break;
+      
+   //    case "Sábado":
+   //    case "Domingo":
+   //       console.log("No es un día laboral")
+   //       break;
+   //    default:
+   //       console.log("No has introducido un día de la semana")
+   //       break;
+   // }
+
+   // 12.- Escribe una aplicación con un String que contenga una contraseña cualquiera. Después se te pedirá que introduzcas la contraseña, con 3 intentos. Cuando aciertes ya no pedirá más la contraseña y mostrará un mensaje diciendo “Enhorabuena”. Piensa bien en la condición de salida (3 intentos y si acierta sale, aunque le queden intentos).
+
+   // let password = "123456";
+
+   // let intento;
+
+   // let i = 0;
+
+   // while ((intento !== password) && (i < 3)) {
+   //    i++;
+   //    intento = prompt("Introduce la contraseña");
+   // }
+
+   // if (intento === password) {
+   //    console.log("Enhorabuena");
+   // } else {
+   //    console.log("Constraseña incorrecta");
+   // }
+
+   // 13.- Crea una aplicación llamada CalculadoraInversa, nos pedirá 2 operandos (int) y un signo aritmético (String), según este último se realizará la operación correspondiente. Al final mostrara el resultado en un cuadro de dialogo.
+   // Los signos aritméticos disponibles son:
+   // +: suma los dos operandos.
+   // -: resta los operandos.
+   // *: multiplica los operandos.
+   // /: divide los operandos, este debe dar un resultado con decimales (double)
+   // ^: 1º operando como base y 2º como exponente.
+   // %: módulo,
+
+//    function CalculadoraInversa() {
+//       let num1 = parseInt(prompt("Introduce el primer número"));
+//       let signo = prompt("Introduce el tipo de operación");
+//       let num2 = parseInt(prompt("Introduce el segundo número"));
+    
+//       if (isNaN(num1) || isNaN(num2)) {
+//          alert("Introduce un número correcto")
+//       } else {
+//       switch (signo) {
+//          case "+":
+//             alert(num1+num2)
+//             break;
+
+//          case "-":
+//             alert(num1-num2)
+//             break;
+
+//          case "x":
+//             alert(num1*num2)
+//             break;
+         
+//          case "/":
+//             alert(num1/num2)
+//             break;
+
+//          case "^":
+//             alert(math.pow(num1,num2))
+//             break;
+
+//          case "%":
+//             alert(num1%num2)
+//          default:
+//             alert("Introduce un signo válido")
+//             break;
+//       }
+//    }
+// }
+
+//    CalculadoraInversa()
